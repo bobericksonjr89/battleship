1602683a6ffe6398ba4b60bbff594706b1120ecc
Robert Erickson <bobericksonjr@gmail.com>
2021-08-09 15:51:25 -0400

    general game flow established

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 /*!********************!*\
   !*** ./src/DOM.js ***!
   \********************/
-/***/ (() => {
+/***/ ((module) => {
 
 const DOM = (() => {
   // DOM capture
@@ -17,8 +17,8 @@ const DOM = (() => {
       for (let j = 0; j < 10; j++) {
         let playerSpace = document.createElement("div");
         playerSpace.classList.add("player-info__player-space");
-        playerSpace.dataset.X = i;
-        playerSpace.dataset.Y = j;
+        playerSpace.dataset.x = j;
+        playerSpace.dataset.y = i;
         playerBoard.appendChild(playerSpace);
       }
     }
@@ -29,18 +29,75 @@ const DOM = (() => {
       for (let j = 0; j < 10; j++) {
         let gameSpace = document.createElement("div");
         gameSpace.classList.add("game-area__gameboard-space");
-        gameSpace.dataset.X = i;
-        gameSpace.dataset.Y = j;
+        gameSpace.dataset.x = j;
+        gameSpace.dataset.y = i;
         gameBoard.appendChild(gameSpace);
       }
     }
+  }
+
+  function colorPlayerSpace(boat, x, y, direction) {
+    let playerSpace;
+
+    for (let i = 0; i < boat.length; i++) {
+      if (direction === "horizontal") {
+        playerSpace = document.querySelector(
+          `.player-info__player-space[data-x='${x + i}'][data-y='${y}']`
+        );
+        playerSpace.classList.add("player-info__player-space--filled");
+      }
+      if (direction === "vertical") {
+        playerSpace = document.querySelector(
+          `.player-info__player-space[data-x='${x}'][data-y='${y + i}']`
+        );
+        playerSpace.classList.add("player-info__player-space--filled");
+      }
+    }
+  }
+
+  function displayHit(x, y) {
+    const space = document.querySelector(
+      `.game-area__gameboard-space[data-x='${x}'][data-y='${y}']`
+    );
+    space.classList.add("game-area__gameboard-space--hit");
+  }
+
+  function displayMiss(x, y) {
+    const space = document.querySelector(
+      `.game-area__gameboard-space[data-x='${x}'][data-y='${y}']`
+    );
+    space.classList.add("game-area__gameboard-space--miss");
+  }
+
+  function displayPlayerHit(x, y) {
+    const space = document.querySelector(
+      `.player-info__player-space[data-x='${x}'][data-y='${y}']`
+    );
+    space.classList.add("player-info__player-space--hit");
+  }
+
+  function displayPlayerMiss(x, y) {
+    const space = document.querySelector(
+      `.player-info__player-space[data-x='${x}'][data-y='${y}']`
+    );
+    space.classList.add("player-info__player-space--miss");
   }
 
   //gridDiv.appendChild(document.createElement("div")).classList.add("grid-box");
 
   displayPlayerBoard();
   displayGameBoard();
+
+  return {
+    colorPlayerSpace,
+    displayHit,
+    displayMiss,
+    displayPlayerHit,
+    displayPlayerMiss,
+  };
 })();
+
+module.exports = DOM;
 
 
 /***/ }),
@@ -49,9 +106,7 @@ const DOM = (() => {
 /*!**************************!*\
   !*** ./src/Gameboard.js ***!
   \**************************/
-/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
-
-const Ship = __webpack_require__(/*! ./Ship.js */ "./src/Ship.js");
+/***/ ((module) => {
 
 const Gameboard = () => {
   const board = [];
@@ -145,7 +200,7 @@ const Player = () => {
     do {
       randomX = randomInt();
       randomY = randomInt();
-    } while (moves.includes({ x: randomX, y: randomY }));
+    } while (moves.some((move) => move.x === randomX && move.y === randomY));
     moves.push({ x: randomX, y: randomY });
 
     return { x: randomX, y: randomY };
@@ -208,6 +263,117 @@ module.exports = Ship;
 // isSunk returns true or false;
 
 
+/***/ }),
+
+/***/ "./src/app.js":
+/*!********************!*\
+  !*** ./src/app.js ***!
+  \********************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+const Player = __webpack_require__(/*! ./Player.js */ "./src/Player.js");
+const Gameboard = __webpack_require__(/*! ./Gameboard.js */ "./src/Gameboard.js");
+const Ship = __webpack_require__(/*! ./Ship.js */ "./src/Ship.js");
+const DOM = __webpack_require__(/*! ./DOM.js */ "./src/DOM.js");
+
+const app = (() => {
+  // DOM Capture
+  const enemySpaces = document.querySelectorAll(".game-area__gameboard-space");
+
+  // Event Handlers
+  enemySpaces.forEach((space) => space.addEventListener("click", attack));
+
+  // assign player1
+  const player1 = Player();
+  // assign player2
+  const player2 = Player();
+  // init turn variable
+  let turn = 1;
+  // player 1 places ships
+  // player 2 places ships
+  // (preselected for now...)
+  function placeShips() {
+    player1.playerBoard.placeShip(player1.patrolBoat, 2, 4, "vertical");
+    DOM.colorPlayerSpace(player1.patrolBoat, 2, 4, "vertical");
+    player1.playerBoard.placeShip(player1.submarine, 1, 0, "horizontal");
+    DOM.colorPlayerSpace(player1.submarine, 1, 0, "horizontal");
+    player1.playerBoard.placeShip(player1.destroyer, 8, 3, "vertical");
+    DOM.colorPlayerSpace(player1.destroyer, 8, 3, "vertical");
+    player1.playerBoard.placeShip(player1.battleship, 4, 4, "vertical");
+    DOM.colorPlayerSpace(player1.battleship, 4, 4, "vertical");
+    player1.playerBoard.placeShip(player1.carrier, 1, 2, "horizontal");
+    DOM.colorPlayerSpace(player1.carrier, 1, 2, "horizontal");
+
+    player2.playerBoard.placeShip(player2.patrolBoat, 0, 0, "vertical");
+    player2.playerBoard.placeShip(player2.submarine, 4, 7, "vertical");
+    player2.playerBoard.placeShip(player2.destroyer, 5, 1, "horizontal");
+    player2.playerBoard.placeShip(player2.battleship, 2, 2, "vertical");
+    player2.playerBoard.placeShip(player2.carrier, 5, 5, "horizontal");
+  }
+
+  // gameflow starts
+  function gameFlow() {
+    // turn++;
+    // odd turns are player 1
+    //DO WHILE??
+    if (turn % 2 === 1) {
+    }
+  }
+
+  function attack(e) {
+    if (turn % 2 !== 1) {
+      return;
+    }
+    const xCoord = e.target.dataset.x;
+    const yCoord = e.target.dataset.y;
+    const result = player1.attackBoard(player2.playerBoard, xCoord, yCoord);
+    if (result === "hit") {
+      DOM.displayHit(xCoord, yCoord);
+    }
+    if (result === "miss") {
+      DOM.displayMiss(xCoord, yCoord);
+    }
+    turn++;
+    setTimeout(AIMove, 500);
+  }
+
+  function AIMove() {
+    const AICoords = player2.generateCoordinates();
+    const AIResult = player2.attackBoard(
+      player1.playerBoard,
+      AICoords.x,
+      AICoords.y
+    );
+    if (AIResult === "hit") {
+      DOM.displayPlayerHit(AICoords.x, AICoords.y);
+    }
+    if (AIResult === "miss") {
+      DOM.displayPlayerMiss(AICoords.x, AICoords.y);
+    }
+    turn++;
+  }
+
+  // even turns are player 2
+  // 34 turns minimum before win condition could be met
+  // player selects space
+  // returns hit or miss
+  // DOM displays result
+  // "sink ship" if applicable
+  // DOM displays sunked ship
+  // if turn >= 34, check if game over
+  // if game over, display winner
+  // click to restart game
+  //  reset turns, reset players, place ships, start the flow again
+
+  placeShips();
+  gameFlow();
+
+  return { attack };
+})();
+
+module.exports = app;
+
+
 /***/ })
 
 /******/ 	});
@@ -237,68 +403,12 @@ module.exports = Ship;
 /******/ 	}
 /******/ 	
 /************************************************************************/
-var __webpack_exports__ = {};
-// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
-(() => {
-/*!********************!*\
-  !*** ./src/app.js ***!
-  \********************/
-const Player = __webpack_require__(/*! ./Player.js */ "./src/Player.js");
-const Gameboard = __webpack_require__(/*! ./Gameboard.js */ "./src/Gameboard.js");
-const Ship = __webpack_require__(/*! ./Ship.js */ "./src/Ship.js");
-const DOM = __webpack_require__(/*! ./DOM.js */ "./src/DOM.js");
-
-const app = (() => {
-  // assign player1
-  const player1 = Player();
-  // assign player2
-  const player2 = Player();
-  // init turn variable
-  let turn = 0;
-  // player 1 places ships
-  // player 2 places ships
-  // (preselected for now...)
-  function placeShips() {
-    player1.playerBoard.placeShip(player1.patrolBoat, 2, 4, "vertical");
-    player1.playerBoard.placeShip(player1.submarine, 1, 0, "horizontal");
-    player1.playerBoard.placeShip(player1.destroyer, 8, 3, "vertical");
-    player1.playerBoard.placeShip(player1.battleship, 4, 4, "vertical");
-    player1.playerBoard.placeShip(player1.carrier, 1, 2, "horizontal");
-
-    player2.playerBoard.placeShip(player2.patrolBoat, 0, 0, "vertical");
-    player2.playerBoard.placeShip(player2.submarine, 4, 7, "vertical");
-    player2.playerBoard.placeShip(player2.destroyer, 5, 1, "horizontal");
-    player2.playerBoard.placeShip(player2.battleship, 2, 2, "vertical");
-    player2.playerBoard.placeShip(player2.carrier, 5, 5, "horizontal");
-  }
-
-  // gameflow starts
-  function gameFlow() {
-    turn++;
-    // odd turns are player 1
-    //DO WHILE??
-    if (turn % 2 === 1) {
-    }
-  }
-
-  // even turns are player 2
-  // 34 turns minimum before win condition could be met
-  // player selects space
-  // returns hit or miss
-  // DOM displays result
-  // "sink ship" if applicable
-  // DOM displays sunked ship
-  // if turn >= 34, check if game over
-  // if game over, display winner
-  // click to restart game
-  //  reset turns, reset players, place ships, start the flow again
-
-  placeShips();
-  gameFlow();
-})();
-
-})();
-
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/app.js");
+/******/ 	
 /******/ })()
 ;
 //# sourceMappingURL=main.js.map
